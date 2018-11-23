1ebaad72dc203c5f8b445666238b6fbbd88623a7
Amir <amir.anwar.said@gmail.com>
2018-11-23 17:00:10 +0200

    Add Product model

--- best_deals_django/base/models.py
+++ best_deals_django/base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Meta:
+        unique_together = ('product_id', 'platform')
+
+    product_id = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    link = models.URLField()
+    old_price = models.CharField(max_length=255)
+    new_price = models.CharField(max_length=255)
+    image = models.URLField()
+    platform = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.platform + ":" + self.description
+
