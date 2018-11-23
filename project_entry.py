b1f56f60ddc1c91b17ece94d8246265016fbdb03
Amir <amir.anwar.said@gmail.com>
2018-11-23 20:09:52 +0200

    add project_entry script

--- project_entry.py
+++ project_entry.py
@@ -1,17 +1,24 @@
-from scrapy.crawler import CrawlerProcess
-from best_deals_scrapy.best_deals_scrapy.spiders.spiders import JumiaSpider, SouqSpider
+import schedule
+import subprocess
+import os
+from multiprocessing import Process
 
+def schedule_and_run_crawling():
+    task = lambda : subprocess.Popen(['python', 'crawl_and_update_db.py'])
+    schedule.every(5).minutes.do(task)
+    while True:
+        schedule.run_pending()
 
-def crawl_spiders():
-    process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE; 7.0; Windows NT 5.1)'})
-    process.crawl(JumiaSpider)
-    process.crawl(SouqSpider)
-    process.start()
-
-
-def main():
-    crawl_spiders()
-
+def run_django_server():
+    subprocess.Popen(['python', 'manage.py', 'runserver'])
 
 if __name__ == "__main__":
-    main()
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    p1 = Process(target=run_django_server)
+    p2 = Process(target=schedule_and_run_crawling)
+    
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
