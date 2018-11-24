d0f16c50ed34684695685eca0b41e72a5cf65554
Amir <amir.anwar.said@gmail.com>
2018-11-24 17:29:12 +0200

    add price filtering feature to API

--- best_deals_django/base/views.py
+++ best_deals_django/base/views.py
@@ -12,15 +12,20 @@ class ProductViewSet(viewsets.ModelViewSet):
         platform = self.request.query_params.get('platform', None)
         brand = self.request.query_params.get('brand', None)
         search = self.request.query_params.get('search', None)
-        
+        max_price = self.request.query_params.get('max_price', None)
+        min_price = self.request.query_params.get('min_price', None)
         sort = self.request.query_params.get('sort', None)
+        
         if platform:
-            queryset = queryset.filter(platform=platform.capitalize()).order_by('description')
+            queryset = queryset.filter(platform__icontains=platform).order_by('description')
         if brand:
             queryset = queryset.filter(brand__icontains=brand).order_by('description')
         if search:
             queryset = queryset.filter(description__search=search).order_by('description')
-        
+        if max_price:
+            queryset = queryset.exclude(new_price__gt=max_price).order_by('description')
+        if min_price:
+            queryset = queryset.exclude(new_price__lt=min_price).order_by('description')
         if sort:
             if sort == 'a':
                 queryset = queryset.order_by('new_price')
