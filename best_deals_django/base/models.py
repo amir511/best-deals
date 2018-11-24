841be758477355cd66c563274f41811ef6b19130
Amir <amir.anwar.said@gmail.com>
2018-11-24 15:43:39 +0200

    multiple error fixing due to different data forms

--- best_deals_django/base/models.py
+++ best_deals_django/base/models.py
@@ -2,16 +2,13 @@ from django.db import models
 
 
 class Product(models.Model):
-    class Meta:
-        unique_together = ('product_id', 'platform')
-
     product_id = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
     brand = models.CharField(max_length=255)
-    link = models.URLField()
+    link = models.URLField(max_length=450)
     old_price = models.CharField(max_length=255)
     new_price = models.CharField(max_length=255)
-    image = models.URLField()
+    image = models.URLField(max_length=450)
     platform = models.CharField(max_length=255)
 
     def __str__(self):
