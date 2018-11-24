8023be2f849fb80825e055cf647d7f96c11b3b2a
Amir <amir.anwar.said@gmail.com>
2018-11-24 16:55:45 +0200

    Extend filtering and search

--- best_deals_django/base/views.py
+++ best_deals_django/base/views.py
@@ -8,12 +8,25 @@ class ProductViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
 
     def get_queryset(self):
-        queryset = Product.objects.all().order_by('description')
+        queryset = Product.objects.all()
         platform = self.request.query_params.get('platform', None)
+        brand = self.request.query_params.get('brand', None)
         search = self.request.query_params.get('search', None)
+        
+        sort = self.request.query_params.get('sort', None)
         if platform:
-            queryset = queryset.filter(platform=platform.capitalize())
+            queryset = queryset.filter(platform=platform.capitalize()).order_by('description')
+        if brand:
+            queryset = queryset.filter(brand__icontains=brand).order_by('description')
         if search:
-            queryset = queryset.filter(description__search=search)
+            queryset = queryset.filter(description__search=search).order_by('description')
+        
+        if sort:
+            if sort == 'a':
+                queryset = queryset.order_by('new_price')
+            elif sort == 'd':
+                queryset = queryset.order_by('-new_price')
+
+
         return queryset
 
